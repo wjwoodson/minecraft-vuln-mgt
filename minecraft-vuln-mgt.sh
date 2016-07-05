09690f7f8b8b13d94103147b86d80e34b6f5a024
Will Woodson <wjwoodson@gmail.com>
2016-07-05 16:33:17 +0000

    updated restore world and began minecraft-vuln-mgt.sh wrapper

--- minecraft-vuln-mgt.sh
+++ minecraft-vuln-mgt.sh
@@ -4,18 +4,8 @@ MCPATH=$HOME'/minecraft'
 WORLDNAME='minecraft-vuln-mgt'
 SCREENNAME='minecraft'
 
-BACKUPFILE=`ls -t $MCPATH'/'$WORLDNAME-*.tgz | head -n1`
-TSARG=$MCPATH'/'$WORLDNAME'-'$1'.tgz'
-
-if [ $1 ] && [ -a $TSARG ] ; then
-	BACKUPFILE=$TSARG	
-fi
-
 echo $DATE' restoring world from backup '$BACKUPFILE
 screen -S $SCREENNAME -X stuff '/say '$DATE' restoring world from backup '$BACKUPFILE$(echo -ne '\015')
-sleep 5
 
-$MCPATH/stop-minecraft.sh
-rm -rf $MCPATH'/'$WORLDNAME
-tar -C $MCPATH -xzf $BACKUPFILE
-$MCPATH/start-minecraft.sh
+#echo $DATE' restoring world from backup '$BACKUPFILE
+#screen -S $SCREENNAME -X stuff '/say '$DATE' restoring world from backup '$BACKUPFILE$(echo -ne '\015')
