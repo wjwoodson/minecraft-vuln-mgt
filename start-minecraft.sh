78b5746639f6dbd12d6d0c6b8288062784675817
Will Woodson <wjwoodson@gmail.com>
2016-08-10 20:08:46 +0000

    Set memory to 2048M to improve performance

--- start-minecraft.sh
+++ start-minecraft.sh
@@ -4,4 +4,4 @@ JARFILE='minecraft_server.1.10.jar'
 SCREENNAME='minecraft'
 
 screen -S $SCREENNAME -d -m
-screen -S $SCREENNAME -X stuff 'java -Xmx1024M -Xms1024M -jar '$MCPATH'/'$JARFILE' nogui'$(echo -ne '\015')
+screen -S $SCREENNAME -X stuff 'java -Xmx2048M -Xms2048M -jar '$MCPATH'/'$JARFILE' nogui'$(echo -ne '\015')
